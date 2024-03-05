c558a5d5462d0dfa1a7ce074e550b31366fd97d8
Harikrishnaveni <harikrishnaveni.s@pillir.io>
2024-03-05 16:37:25 +0530

    build file changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   e2e: {
     reporter: 'cypress-mochawesome-reporter',
-    reporterOptions: {
-      charts: true,
-      reportPageTitle: 'AutomationTestReport',
-      embeddedScreenshots: true,
-      inlineAssets: true,
-      saveAllAttempts: false,
-    },
+    // reporterOptions: {
+    //   charts: true,
+    //   reportPageTitle: 'AutomationTestReport',
+    //   embeddedScreenshots: true,
+    //   inlineAssets: true,
+    //   saveAllAttempts: false,
+    // },
     setupNodeEvents(on, config) {
       // implement node event listeners here
       require('cypress-mochawesome-reporter/plugin')(on);
